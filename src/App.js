61771aa60219c2318ebcdd9ccccc15490efaf4ce
Curtis <41402426+curtisadams28@users.noreply.github.com>
2023-05-01 16:52:35 +1200

    Basic styling for hero section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Navigation from './components/navigation/navigation';
 import Button from './components/button/button';
 
+import { ReactComponent as Mail } from './img/mail.svg';
+import { ReactComponent as Linkedin } from './img/linkedin.svg';
+import { ReactComponent as Github } from './img/github.svg';
+
 function App() {
   return (
     <div className="app">
@@ -11,16 +15,35 @@ function App() {
           <h1 className='hero-heading'>Curtis Adams</h1>
           <p className='hero-subheading'>Front End Developer</p>
         </div>
-        <div className='background-gradient'>
+        <div className='hero-contact'>
+          <div className='hero-contact-inner'>
+            <div className='social-icons'>
+            <div className='vl'></div>
+            <a href="#">
+                <Mail />
+              </a>
+              <a href="#">
+                <Linkedin />
+              </a>
+              <a href="#">
+                <Github />
+              </a>
+
+            </div>
+            <div className='email'>
+              <div className='vl'></div>
+              <p>curtisadams28@gmail.com</p>
+            </div>
+          </div>
 
         </div>
+        <div className='background-gradient'></div>
 
       </div>
       <div className='my-work-section section'>
         <div className='section-content hero-content'>
           <h1 className='heading'>My Work</h1>
         </div>
-
       </div>
     </div>
   );
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,17 +1,24 @@
 import './navigation.scss';
+import { ReactComponent as Clogo } from '../../img/c-logo.svg';
 
 import Button from '../button/button';
 
 function Navigation() {
   return(
     <nav className='nav'>
+      <div className='nav-inner'>
+        <Clogo className='c-logo'/>
         <ul className='nav-links'>
-          <li>Home</li>
-          <li>Work</li>
-          <li>Skills</li>
-          <li>Contact</li>
-        </ul>
-        <Button className='btn btn-nav' text='Resumé'/>
+            <li>Home</li>
+            <li>Work</li>
+            <li>Skills</li>
+            <li>Contact</li>
+          </ul>
+          <Button className='btn btn-nav' text='Resumé'/>
+      </div>
+
+        
+        
     </nav>
   );
 }
