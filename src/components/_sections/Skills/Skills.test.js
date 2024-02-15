6470432fea502badffce8df96027aed825b3ec4d
Curtis <41402426+curtisadams28@users.noreply.github.com>
2024-02-15 14:04:04 +1300

    Added section id to WorkDetails

---
+++ src/components/_sections/Skills/Skills.test.js
@@ -0,0 +1,4 @@
+import Skills from './Skills';
+describe(Skills, () => {
+  it('')
+})
--- src/components/_sections/WorkDetails/WorkDetails.js
+++ src/components/_sections/WorkDetails/WorkDetails.js
@@ -12,42 +12,44 @@ function WorkInfoPage(props) {
 
   const pageData = PageContent.myWorkArray[props.index];
   return (
-    <div className='work-page section'>
-      <div className='section-content'>
-        <h1 className='heading'>{pageData.title}</h1>
-        <button className='close-info-page-button' onClick={() => props.handleCardClick(null)}>
-          <LeftArrow />
-          <p>Back to projects</p>
-        </button>
-        {pageData.imageFile &&
-          <img
-            className={`img-fade-in ${imageLoaded ? "img-loaded" : ""}`}
-            src={require(`../../../img/${pageData.imageFile}`)}
-            alt=""
-            onLoad={() => setImageLoaded(true)}
-          />
-        }
-        <div className='info-section'>
-          <h2 className='heading-sm'>About</h2>
-          <p>{pageData.about}</p>
-        </div>
-        <div className='info-section'>
-          <h2 className='heading-sm'>Technologies</h2>
-          <Tags tagArray={pageData.technologies} />
-        </div>
-        <div className='info-section'>
-          <h2 className='heading-sm'>Github</h2>
-          <a href={pageData.githubLink} target='_blank'>{pageData.github}</a>
-        </div>
-        {
-          pageData.website &&
+    <section id='work'>
+      <div className='work-page section'>
+        <div className='section-content'>
+          <h1 className='heading'>{pageData.title}</h1>
+          <button className='close-info-page-button' onClick={() => props.handleCardClick(null)}>
+            <LeftArrow />
+            <p>Back to projects</p>
+          </button>
+          {pageData.imageFile &&
+            <img
+              className={`img-fade-in ${imageLoaded ? "img-loaded" : ""}`}
+              src={require(`../../../img/${pageData.imageFile}`)}
+              alt=""
+              onLoad={() => setImageLoaded(true)}
+            />
+          }
           <div className='info-section'>
-          <h2 className='heading-sm'>Website</h2>
-          <a href={pageData.websiteLink} target='_blank'>{pageData.website}</a>
+            <h2 className='heading-sm'>About</h2>
+            <p>{pageData.about}</p>
           </div>
-        }
+          <div className='info-section'>
+            <h2 className='heading-sm'>Technologies</h2>
+            <Tags tagArray={pageData.technologies} />
+          </div>
+          <div className='info-section'>
+            <h2 className='heading-sm'>Github</h2>
+            <a href={pageData.githubLink} target='_blank'>{pageData.github}</a>
+          </div>
+          {
+            pageData.website &&
+            <div className='info-section'>
+            <h2 className='heading-sm'>Website</h2>
+            <a href={pageData.websiteLink} target='_blank'>{pageData.website}</a>
+            </div>
+          }
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
